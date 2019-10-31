81d7c13f3ee92a776beba8947a429e9cbcdcdc0e
geraldiner <hello@geraldiner.com>
2019-10-31 12:53:31 -1000

    fixed color issues, collisions, added character sprites

--- main.lua
+++ main.lua
@@ -9,9 +9,10 @@ local bumpWorld = bump.newWorld()
 -- Blocks aka 'collisions' functions
 local blocks = {}
 
-local function drawBlock(box, r, g, b)
-	love.graphics.setColor(r, g, b, 70)
+local function drawBlock(box, r, g, b, o)
+	love.graphics.setColor(r, g, b, o)
 	love.graphics.rectangle("fill", box.x, box.y, box.w, box.h)
+	love.graphics.reset()
 end
 
 local function addBlock(name, x, y, w, h, type, destination)
@@ -21,54 +22,67 @@ local function addBlock(name, x, y, w, h, type, destination)
 end
 
 local function drawBlocks()
-  for _,block in ipairs(blocks) do
-    if block.type == 'door' then
-      drawBlock(block, 255, 0, 0)
-    elseif block.type == 'wall' then
-      drawBlock(block, 0, 0, 255)
-    elseif block.type == 'npc' then
-      drawBlock(block, 255, 0, 255)
-    end
-  end
+	for _,block in ipairs(blocks) do
+		if block.type == 'door' then
+			drawBlock(block, 0, 0, 0, 0)
+		elseif block.type == 'wall' then
+			drawBlock(block, 0, 0, 0, 0)
+		elseif block.type == 'npc' then
+			drawBlock(block, 255, 0, 255, 100)
+		end
+	end
 end
 
 -- Player functions
-local player = {x = 96, y = 96, dx = 0, dy = 0, w = 16, h = 32, speed = 2.5, isMoving = true, party = {}}
+local sprite = love.graphics.newImage("assets/sprites/witch.png")
+local player = {sprite = sprite, x = 96, y = 96, dx = 0, dy = 0, w = 16, h = 16, speed = 1.5, isMoving = true, party = {}}
 
 local function updatePlayer(dt)
-  local speed = player.speed
-  if player.isMoving then
-  	--print(player.x, player.y)
-    local dx, dy = 0, 0
-    if love.keyboard.isDown('right') then
-      dx = speed
-    elseif love.keyboard.isDown('left') then
-      dx = -speed
-    elseif love.keyboard.isDown('down') then
-      dy = speed
-    elseif love.keyboard.isDown('up') then
-      dy = -speed
-    end
-    player.dx, player.dy = dx, dy
-    if dx ~= 0 or dy ~= 0 then
-      player.x, player.y, cols, cols_len = bumpWorld:move(player, player.x + dx, player.y + dy)
-      for i=1, cols_len do
-        local col = cols[i].other
-        print(col.type)
-		if col.type == 'door' then
-			return col
+	local speed = player.speed
+	if player.isMoving then
+	--print(player.x, player.y)
+		local dx, dy = 0, 0
+		if love.keyboard.isDown('right') then
+			dx = speed
+		elseif love.keyboard.isDown('left') then
+			dx = -speed
+		elseif love.keyboard.isDown('down') then
+			dy = speed
+		elseif love.keyboard.isDown('up') then
+			dy = -speed
 		end
-      end
-    end
-  else
-    bumpWorld:update(player,player.x, player.y)
-  end
+		player.dx, player.dy = dx, dy
+		
+		if dx ~= 0 or dy ~= 0 then
+			player.x, player.y, cols, cols_len = bumpWorld:move(player, player.x + dx, player.y + dy)
+			for i=1, cols_len do
+				local col = cols[i].other		
+				if col.type == 'door' then
+					if col.name == 'South' then
+						if col.destination == 'exit' then
+							showEndGame()
+							break
+						else
+							player.x, player.y = 16*8, 16
+						end
+					elseif col.name == 'North' then
+						player.x, player.y = 16*8, 16*13
+					elseif col.name == 'West' then
+						player.x, player.y = 16*13, 16*8
+					elseif col.name == 'East' then
+						player.x, player.y = 16, 16 *8
+					end
+					return col
+				else
+					return nil
+				end
+			end
+		end
+	end
 end
 
 local function drawPlayer()
-  	love.graphics.setColor(0, 255, 0)
-    love.graphics.rectangle("fill", player.x, player.y, player.w, player.h/2)
-	love.graphics.setColor(0,0,0,0)
+	love.graphics.draw(player.sprite, player.x, player.y)
 end
 
 local room = {}
@@ -95,7 +109,7 @@ local npc = {x = 16*11, y = 64, w = 16, h = 32, speed = 2.5 }
 
 -- Main L�VE functions
 function love.load()
-	bumpWorld:add(player, player.x, player.y, player.w, player.h/2)
+	bumpWorld:add(player, player.x, player.y, 16, 16)
 	room = World:getRoom('study')
 	removeBlocks()
 	addBlocks(room)
@@ -103,73 +117,78 @@ function love.load()
 end
 
 function love.update(dt)
-  local event = updatePlayer(dt)
-  if event and event.type == 'door' then
-	bumpWorld:update(player,16*8, 16*8)
-	
-	removeBlocks()
-    room = World:getRoom(event.destination)
-    addBlocks(room)
-    --if event.name == 'South' then
-    --   player.x, player.y = 16*8, 16
-    --elseif event.name == 'North' then
-    --   player.x, player.y = 16*8, 16*13
-    --elseif event.name == 'West' then
-    --   player.x, player.y = 16*13, 16*8
-    --elseif event.name == 'East' then
-    --   player.x, player.y = 16, 16 *8
-    --end
-    
-    
-  end
+	local event = updatePlayer(dt)
+	if event and event.type == 'door' then
+		bumpWorld:update(player, player.x, player.y)
+		removeBlocks()
+		room = World:getRoom(event.destination)
+		addBlocks(room)
+	end
+	talkies.update(dt)
 end
 
 function love.draw()
 	room.map:drawTileLayer('Floor')
 	room.map:drawTileLayer('InnerWalls')
+	drawPlayer()
 	room.map:drawTileLayer('OuterWalls')
 	drawBlocks()
-	drawPlayer()
 	talkies.draw()
 end
 
 function addToParty(item)
-  player.party[#player.party+1] = item.name
-  bumpWorld:remove(item)
-  for i=1, #player.party do
-    print(player.party[i])
-  end
+	player.party[#player.party+1] = item.name
+	blocks[table.indexOf(blocks,item)] = nil
+	bumpWorld:remove(item)
 end
 
-
+local dialog = nil
 function love.keypressed(key)
-  if key == "return" then
-    if player.isMoving then
-      local items, len = bumpWorld:queryRect(player.x-1,player.y-1,player.w+2,player.h+2)
-      for i=1, len do
-        if items[i].name == 'npc' then
-          player.isMoving = false
-          talkies.say(
-            "NPC",
-            {"Lets go home", "Do you want to add npc to your party?"},
-            {
-              titleColor = {1,0,1},
-              options={
-                {"Yes", function() addToParty(items[i]) end},
-                {"No", function() return end}
-              },
-              oncomplete = function() return end
-            }
-          )
+	if key == "return" then
+		if player.isMoving then --player is moving, want to detect npcs
+			local items, len = bumpWorld:queryRect(player.x-1,player.y-1,player.w+2,player.h*2+2)
+			for i=1, len do
+				if items[i].name == 'npc' then --npc detected
+					player.isMoving = false --stop moving
+					dialog = talkies.say( --start talking
+						"NPC",
+						{"Lets go home", "Do you want to add npc to your party?"},
+						{
+							titleColor = {1,0,1},
+							textSpeed = fast,
+							options={
+								{"Yes", function() addToParty(items[i])return end},
+								{"No", function() return end}
+							},
+							onmessage=function() talkies.onAction() end,
+							onmessage=talkies.onAction(),
+							oncomplete=function() 
+								dialog = nil
+								talkies.clearMessages()
+								player.isMoving = true
+							end
+						}
+					)
+				end
+			end
+		else --player not moving
+			talkies.onAction()
+		end
+	elseif key == "up" then talkies.prevOption()
+	elseif key == "down" then talkies.nextOption()
+	end
+end
+
+function showEndGame()
+	print("THE GAME HAS ENDED")
+end
+
+function table.indexOf(t, object)
+    if type(t) ~= "table" then error("table expected, got " .. type(t), 2) end
+
+    for i, v in pairs(t) do
+        if object == v then
+            return i
         end
-      end
-    else
-      talkies.onAction()
-      talkies.clearMessages()
-      player.isMoving = true
     end
-  elseif key == 'space' then talkies.onAction()
-  elseif key == "up" then talkies.prevOption()
-  elseif key == "down" then talkies.nextOption()
-  end
 end
